7e64efffabe55c98f5281f49cc3d7e9e158f45d4
rubengaal <63323420+gaalruben@users.noreply.github.com>
2022-08-16 14:37:05 +0200

    Added temporal io workflow interfaces
    Co-Authored-By: huanamanh <62505592+huanamanh@users.noreply.github.com>

--- deepface/src/main/java/com/deepface/deepface/DeepfaceApplication.java
+++ deepface/src/main/java/com/deepface/deepface/DeepfaceApplication.java
@@ -1,13 +1,36 @@
 package com.deepface.deepface;
 
+import com.deepface.deepface.repository.ImageRepository;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories()
+@ComponentScan("com.deepface.deepface")
 public class DeepfaceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeepfaceApplication.class, args);
 	}
 
+	@Bean
+	public MongoClient mongoClient() {
+		return MongoClients.create("mongodb+srv://deepcheap:pirosbicikli666@deepface.srk0atu.mongodb.net/?retryWrites=true&w=majority");
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate(MongoClient mongoClient) throws Exception {
+		return new MongoTemplate(mongoClient, "databaseName");
+	}
 }
+
+
